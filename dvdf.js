88dc1d18af0153cff6d96e26cc53f66ca5a6e27d
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-03-23 00:10:28 +0700

    Add files via upload

---
+++ dvdf.js
@@ -0,0 +1,119 @@
+[
+{
+ "cauhoi" : "Mình rồng, đuôi phụng le te. Mùa Đông ấp trứng, mùa Hè nở con. (Là gì?).",
+    "a": "A.  Cây cau",
+    "b": "B.  Cây dừa",
+    "c": "C.  Cây bưởi",
+    "d": "D.  Cây ăn thịt người:))",
+    "dapan1": "A",
+    "dapan2": "Cây cau"
+    },
+    {
+    "cauhoi": "Cày trên đồng ruộng trắng phau. Khát xuống uống nước giếng sâu đen ngòm? (Là cái gì?).",
+    "a": "A. Bút bi",
+    "b": "B. Bút mực",
+    "c": "C. Bút Lông gà",
+    "d": "D. Bút chì",
+    "dapan1": "A",
+    "dapan2": "Bút bi"
+    },
+    {
+  "cauhoi": "Hè về áo đỏ như son. Hè đi thay lá xanh non mượt mà. Bao nhiêu tay toả rộng ra. Như vẫy như đón bạn ta đến trường. (Là cây gì?).",
+    "a": "A. Cam",
+    "b": "B. Đào",
+    "c": "C. Phượng",
+    "d": "D. Nhãn",
+    "dapan1": "C",
+    "dapan2": "Phượng"
+    },
+    {
+    "cauhoi": "Đố ai cũng khách thoa quần. Đạp luồng sóng dữ, đuổi quân giặc thù. Cửu Chân nức tiếng ngàn thu. Vì dân quyết phá ngục tù lầm than? Là ai?",
+    "a": "A. Bác Hồ",
+    "b": "B. Lạc Long Quân",
+    "c": "C. Thánh Gióng",
+    "d": "D. Bà Triệu",
+    "dapan1": "D",
+    "dapan2": "Bà Triệu"
+    },
+    {
+    "cauhoi": "Bốn cột tứ trụ. Người ngự lên trên. Gươm bạc hai bên. Chầu vua thượng đế. Là gì?",
+    "a": "A. Con voi",
+    "b": "B. Con chó",
+    "c": "C. Con mèo",
+    "d": "D. Con ngựa",
+    "dapan1": "A",
+    "dapan2": "Con voi"
+    },
+    {
+    "cauhoi": "Thân dài thượt. Ruột thẳng băng. Khi thịt bị cắt khỏi chân. Thì ruột lòi dần vẫn thẳng như rươi? (Là cái gì?).",
+    "a": "A. Bút bi",
+    "b": "B. Bút chì",
+    "c": "C. Bút mực",
+    "d": "D. Bút lông",
+    "dapan1": "B",
+    "dapan2": "Bút chì"
+    },
+    {
+    "cauhoi": "Cây khô một lá bốn năm cành. Đường đi khúc khuỷu nhọc tay anh. Gặp kẻ tiểu nhân buồn chẳng nói. Chờ người quân tử mới dương danh. Là gì?",
+    "a": "A. Cây đàn",
+    "b": "B. Sáo",
+    "c": "C. Trống",
+    "d": "D. Giàn DJ",
+    "dapan1": "A",
+    "dapan2": "Cây đàn"
+    },
+    {
+    "cauhoi": "Cái gì mà đi thì nằm, đứng cũng nằm, nhưng nằm lại đứng? Là gì?",
+    "a": "A. Bàn tay",
+    "b": "B. Tai",
+    "c": "C. Bàn chân",
+    "d": "D. Mắt",
+    "dapan1": "C",
+    "dapan2": "Bàn chân"
+    },
+    {
+    "cauhoi": "Đầu đuôi vuông vắn như nhau. Thân chia nhiều đốt rất mau rất đều. Tính tình chân thực đáng yêu. Muốn biết dài ngắn mọi điều có em? (Là cái gì?).",
+    "a": "A. Bàn",
+    "b": "B. Quyển Vở",
+    "c": "C. Bút",
+    "d": "D. Thước kẻ",
+    "dapan1": "D",
+    "dapan2": "Thước kẻ"
+    },
+    {
+    "cauhoi": "Ở nơi cao nhất trên đầu. Chẳng đen như tóc, lại màu đỏ tươi. Lúc khỏe đẹp như mặt trời. Đến khi đau yếu màu tươi xám dần. Là gì?",
+    "a": "A. Tóc bọn bad",
+    "b": "B. Tai thỏ",
+    "c": "C. Mào gà trống",
+    "d": "D. Bờm ngựa",
+    "dapan1": "C",
+    "dapan2": "Mào gà trống"
+    },
+    {
+    "cauhoi": "Vuông vuông cửa đóng 2 đầu. 100 thằng chệt lần hồi chui ra. Thằng nào không mũ thì tha. Thằng nào có mũ đem ra đốt đầu. Là gì?",
+    "a": "A. Thuốc nổ",
+    "b": "B. Đá :))",
+    "c": "C. Bao diêm",
+    "d": "D. Bao thuốc lá",
+    "dapan1": "C",
+    "dapan2": "Bao diêm"
+    },
+    {
+    "cauhoi": "Có đầu mà chẳng có đuôi. Có một khúc giữa cứng ruôi lại mềm. (Là cái gì?).",
+    "a": "A. Đòn bẩy",
+    "b": "B. Đòn gánh",
+    "c": "C. Que",
+    "d": "D. Gậy",
+    "dapan1": "B",
+    "dapan2": "Đòn gánh"
+    },
+    {
+    "cauhoi": "Thân em xưa ở bụi tre. Mùa đông xếp lại mùa hè mở ra. (Là cái gì?).",
+    "a": "A. Quạt giấy.",
+    "b": "B. Gỗ",
+    "c": "C. Quạt",
+    "d": "D. Quạt lan",
+    "dapan1": "A",
+    "dapan2": "Quạt giấy"
+    }
+  ]
